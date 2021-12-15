8d4a068ee0142e73a9e0d25844a8177ee711cc71
Oulugbek <ulugbekxolmanov419@gmail.com>
2021-12-15 12:48:03 +0500

    new4

---
+++ arithmetic_op03.py
@@ -0,0 +1,3 @@
+number = int(input())
+answer = number//10 + number%10*10
+print(answer)
---
+++ arithmetic_op06.py
@@ -0,0 +1,4 @@
+number = int(input())
+answer = float(number)/2 - number//2
+number = number/2
+print(answer,number)
